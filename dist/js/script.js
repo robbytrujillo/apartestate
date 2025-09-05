311a954867b6969e8c6b90c1561d2a3173722797
robbytrujillo <robbyikusuma88@gmail.com>
2025-09-05 16:17:35 +0700

    Introduction apartestate

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,1 +1,6 @@
-const menuBar = document.querySelector("ri-menu-unfold-3-fill");
+const menuBar = document.querySelector(".ri-menu-unfold-3-fill");
+const menuNav = document.querySelector(".menu");
+
+menuBar.addEventListener("click", () => {
+  menuNav.classList.toggle("menu-active");
+});
